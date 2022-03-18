1669411ed5cf4267f2674dc7840bafa8b37a327f
Alexsander <alexsandergutierreez@gmail.com>
2022-03-18 20:10:57 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,6 +56,7 @@ export class AppComponent {
           horizontalPosition: 'right',
           verticalPosition: 'top',
           duration: 3000,
+          panelClass:'green-snackbar'
         });
         this.pegarDados();
         this.form.reset();
@@ -65,6 +66,7 @@ export class AppComponent {
           horizontalPosition: 'right',
           verticalPosition: 'top',
           duration: 3000,
+          panelClass:'red-snackbar'
         });
       }
     });
